c4c6cf85daf694c2d370a40e0a1cd29cda0d4c8a
Radu Hociung <radu.git@ohmi.org>
2025-01-07 01:18:20 -0500

    Almost ready for release

--- build.sh
+++ build.sh
@@ -57,8 +57,6 @@ error=0
 set -o errexit
 set -o pipefail
 
-ls -la
-
 [[ $error == 0 ]] || {
     >&2 echo "See https://github.com/archivium/unbust"
     exit 1
@@ -82,46 +80,24 @@ CDN_set_vars()  {
         SOURCE_BRANCH=$CF_PAGES_BRANCH
         DEPRECATION_MESSAGE="Published $(sitestats) from ${CF_PAGES_BRANCH} (${CF_PAGES_COMMIT_SHA::8}), at ${CF_PAGES_URL}"
     else
-        # Local testing
-        if [ "${DEBSIGN_KEYID}" == "8F5713F1" ] ; then
-            export GIT_ALTERNATE_OBJECT_DIRECTORIES=$WORKDIR/.git/objects
-            if [ ${TEST_SHA:+isset} ] ; then
-                SOURCE_COMMIT_SHA="$TEST_SHA"
-            fi
-            SOURCE_BRANCH=unknown-local/source-branch
-            DEPRECATION_MESSAGE="Published $(sitestats)"
-        fi
+        >&2 echo "ERROR: This CDN is not yet supported."
+        return 1
+        DEPRECATION_MESSAGE="Published $(sitestats)"
     fi
 }
 # ################ End of CDN support hooks #####################
 
 ENCRYPTION_CIPHER=(-aes-256-cbc -md sha512 -pbkdf2 -iter 1000000 -salt -pass "pass:$UNBUST_CACHE_KEY")
 
-# Local testing
-if [ "${DEBSIGN_KEYID}" == "8F5713F1" ] ; then
-    TEST_SHA="377adeb59f3f3256d7ddee3e2e9ca09361507bd4"
-fi
 
 # FIXME: git repack fails because tree objects are not added to the db
-LINK_PUBLISHED_HISTORY_TO_SOURCE_HISTORY=""
 
 unset SOURCE_COMMIT_SHA
-
-
-# Local testing
-if [ "${DEBSIGN_KEYID}" == "8F5713F1" ] ; then
-    output_dir="/tmp/out"
-    [[ ${output_dir:+isset} ]]
-    rm -rf "${output_dir}"
-else
-    output_dir=out
-fi
 
 # FIXME: Implement deployment to subdir of host (ie, wget needs to --cut-dirs)
 DEFAULT_UNBUST_CACHE_TIME="3 months ago"
 
 NOW=$(date +%s)
-# NOW=$(command date --date="2020-06-01" "+%s")
 
 date() {
     command date --date=@$NOW "$@"
@@ -133,15 +109,13 @@ git() {
 
 deprecation_setup() {
     local dburl="$1"
-    # GIT_DIR=${UNBUST_CACHE_DBNAME:-}
 
     export GIT_COMMITTER_DATE=$NOW GIT_AUTHOR_DATE=$NOW GIT_DIR=.git
 
     # Ensure UNBUST_CACHE_DBNAME is set. There is no default, because it should be
     # secret
-set -x
+
     if [ ! -d "${UNBUST_CACHE_DBNAME}" ] ; then
-        # mkdir -p "${UNBUST_CACHE_DBNAME}"
         command git -c init.defaultBranch=published init --template=/dev/null "${dbdir}"
         git config pack.packSizeLimit 20m
         git config commit.gpgSign false
@@ -149,29 +123,14 @@ set -x
         git config user.name "Archivium Cache Bot"
         git config user.email "unbust-cache-script@archivium.org"
         
-        # git remote add "$remotename" "${dburl}/${UNBUST_CACHE_DBNAME}/$dbdir"
 
-        # Local testing
-        if [ "${DEBSIGN_KEYID}" == "8F5713F1" ] ; then
-            git remote set-url "$remotename" "${output_dir}.public/${UNBUST_CACHE_DBNAME}/$GIT_DIR"
-            rsync -a "${output_dir}.public/${UNBUST_CACHE_DBNAME}/$GIT_DIR" "${UNBUST_CACHE_DBNAME}/"
-        fi
-        # End local testing
-
-        if [ "${DEBSIGN_KEYID}" == "8F5713F1" ] ; then
-            :
+        if ! curl -s --fail "${dburl}/${UNBUST_CACHE_DBNAME}" | 
+            openssl enc "${ENCRYPTION_CIPHER[@]}" -d |
+            tar xjv -C "${dbdir}" ; then
+            git commit --allow-empty -m "Initial commit"
+            git branch empty
         else
-            set -o pipefail
-            if ! curl -s --fail "${dburl}/${UNBUST_CACHE_DBNAME}" | 
-                openssl enc "${ENCRYPTION_CIPHER[@]}" -d |
-                tar xjv -C "${dbdir}" ; then
-            # if true ; then
-                git commit --allow-empty -m "Initial commit"
-                git branch empty
-            else
-                git checkout "$branchname"
-                # git remote remove "$remotename"
-            fi
+            git checkout "$branchname"
         fi
     fi
 }
@@ -195,7 +154,6 @@ deprecation_refill() {
 
     echo "#   Deprecated files (cut-off is $(date --date=@$obstime)):"
     echo "#   -----------------------------------------------------------"
-    # git log --skip=1 -q --since="$obstime" --no-decorate --format="#   %h %s"
 
     local wgetlog=$(mktemp) list=$(mktemp)
     while read -r commit ; do
@@ -207,9 +165,7 @@ deprecation_refill() {
             fi
 
         done < <(git show "$commit":files 2>/dev/null || :)  >$list
-        # echo Files to fetch:
-        # cat $list
-        # echo -- ------------------
+
         if [ -s "$list" ] ; then
             # In case any deprecated file is missing, the deployment should
             # fail. Reliably preserving deprecated files is the script's ONE JOB
@@ -232,23 +188,15 @@ deprecation_refill() {
             # Failing to preserve deprecated files is a failure. This script
             # has ONE JOB, and that is it.
             #
-            # Local testing
-            if [ "${DEBSIGN_KEYID}" == "8F5713F1" ] ; then
-                if ! rsync --files-from=$list $output_dir.public $output_dir ; then
-                    echo "FAILED TO RSYNC"
-                    return 1
-                fi
-            else
-                # FIXME: If through some mapping, the deployed files appear in a subdirectory,
-                # need to cut some directories
-                if ! wget --retry-connrefused --recursive --no-host-directories --input-file=$list --base="$dburl" -o "$wgetlog" ; then
-                    echo "# ##########################################################"
-                    echo "# #######  Failed to download deprecated files: ############"
-                    echo "# ##########################################################"
-                    cat $wgetlog
-                    echo "# ##########################################################"
-                    return 1
-                fi
+            # FIXME: If through some mapping, the deployed files appear in a subdirectory,
+            # need to cut some directories
+            if ! wget --retry-connrefused --recursive --no-host-directories --input-file=$list --base="$dburl" -o "$wgetlog" ; then
+                echo "# ##########################################################"
+                echo "# #######  Failed to download deprecated files: ############"
+                echo "# ##########################################################"
+                cat $wgetlog
+                echo "# ##########################################################"
+                return 1
             fi
             local size=($(sed -z 's/\n/\x00/g' $list | du --files0-from=- -sh))
             local num_files=$(wc -l < $list)
@@ -289,49 +237,13 @@ deprecation_track() {
     find -type f -printf '%P\n' >"${dbdir}"/files
 
     git add -A files
-set -x
-    if [[ ${LINK_PUBLISHED_HISTORY_TO_SOURCE_HISTORY:+isset} && ${SOURCE_COMMIT_SHA:+isset} && ${GIT_ALTERNATE_OBJECT_DIRECTORIES:+isset} ]] ; then
-        # Craft a merge commit, where the source commit will be a merged into
-        # the published history.
-        # You can then fetch the deprecation history into the project repo
-        # and the publication history will be properly linked to the sources.
-        local parent=$(git rev-parse HEAD)
-        local tree=$(git write-tree)
 
-        local commit=$(git commit-tree -p $parent -p $SOURCE_COMMIT_SHA -m "$msg" ${tree})
-        git update-ref -m "$msg" refs/heads/$branchname $commit
-        # git replace --graft $SOURCE_COMMIT_SHA
+    git commit --allow-empty -m "$msg"
 
-        # Copy the commit object from the source repo so rev-parse doesn't fail
-        echo $SOURCE_COMMIT_SHA >> "${dbdir}/$GIT_DIR/shallow"
-        # FIXME: WORKDIR should not be here
-        echo $SOURCE_COMMIT_SHA | command git -C $WORKDIR pack-objects --stdout | GIT_ALTERNATE_OBJECT_DIRECTORIES= git unpack-objects
-        # git log --oneline --graph --decorate --all
-        GIT_ALTERNATE_OBJECT_DIRECTORIES= git cat-file -p $SOURCE_COMMIT_SHA
-        GIT_ALTERNATE_OBJECT_DIRECTORIES= git cat-file -p $branchname
-        cat $dbdir/$GIT_DIR/shallow
-    else
-        git commit -m "$msg" || :
-    fi
-
-# set +x
     git gc --quiet
-    # GIT_ALTERNATE_OBJECT_DIRECTORIES= git repack -adl
     git prune-packed
-        # GIT_ALTERNATE_OBJECT_DIRECTORIES= git cat-file -p $SOURCE_COMMIT_SHA
-        # GIT_ALTERNATE_OBJECT_DIRECTORIES= git cat-file -p $branchname
-
     git pack-refs --all --prune
-        # GIT_ALTERNATE_OBJECT_DIRECTORIES= git cat-file -p $SOURCE_COMMIT_SHA
-        # GIT_ALTERNATE_OBJECT_DIRECTORIES= git cat-file -p $branchname
-
     git update-server-info
-    find 
-
-    # Local testing
-    if [ "${DEBSIGN_KEYID}" == "8F5713F1" ] ; then
-        git log --graph --oneline --all
-    fi
 
     deprecation_refill "${dburl}"
 
@@ -339,10 +251,7 @@ set -x
     # FIXME: Max size 25M, then we have to split
     tar cjf "${UNBUST_CACHE_DBNAME}" --remove-files -C "${dbdir}" "$GIT_DIR" |
        openssl enc "${ENCRYPTION_CIPHER[@]}" -out "${UNBUST_CACHE_DBNAME}"
-    # FIXME: Max size 25M, then we have to split
-    # mv "${UNBUST_CACHE_DBNAME}/$GIT_DIR" "${UNBUST_CACHE_DBNAME}/$dbdir"
 
-    find
     rm -rf $dbdir
 }
 
@@ -352,8 +261,8 @@ CDN_set_vars
 
 command git remote -v
 set -x
-mkdir -p "$output_dir"
-WORKDIR=$PWD
+output_dir=out
+mkdir -p "${output_dir}"
 cd $output_dir
 
 echo hello >"$FN"
@@ -361,39 +270,3 @@ mkdir subdir
 echo hello >"subdir/$FN"
 
 deprecation_track "$PUBLIC_URL" "$DEPRECATION_MESSAGE"
-
-
-# Local testing
-if [ "${DEBSIGN_KEYID}" == "8F5713F1" ] ; then
-    rm -rf "${output_dir}.public"
-    mv "$output_dir" "${output_dir}.public"
-fi
-# End local testing
-
-
-# wrangler whoami
-# 
-# set
-# 
-# git --version
-# wget --version
-# df -h
-# df -h .
-# 
-# dpkg -l
-# 
-# echo FILE TYPE :::::::::::::::::::::::::::::
-# file /opt/build/bin/build
-# 
-# set -x
-# 
-# pstree -apl
-# 
-# ps axuww
-# 
-# echo /opt/pages/build_tool/run_build.sh ::::::::::::::::::::
-# sudo cat /opt/pages/build_tool/run_build.sh
-# 
-# 
-# echo EXITING WITH ERROR
-# exit 2
